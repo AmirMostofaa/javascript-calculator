f0e0fe8fe54b942932c05ec3e92c045df0a49a41
Amir Mostofaa <mostafacox1@gmail.com>
2020-10-16 20:58:26 +0600

    upadated

---
+++ main.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.display');
+
+
+function calculate(event){
+    const clickedButtonValue = event.target.value;
+
+    if(clickedButtonValue === '='){
+        if(display.value !== ''){
+            display.value = eval(display.value)
+        }
+    }else if(clickedButtonValue === 'C'){
+        display.value = '';
+    }else{
+        display.value += clickedButtonValue;
+    }
+}
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', calculate)
+})
